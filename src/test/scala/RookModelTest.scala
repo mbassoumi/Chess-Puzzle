4aab102d298a98f04c19b9ab23ecacb78429cce7
majdbassoumi <mbbassoumi@gmail.com>
2019-05-01 15:29:37 +0300

    rook model test

--- src/test/scala/RookModelTest.scala
+++ src/test/scala/RookModelTest.scala
@@ -1,83 +1,70 @@
-import models.Knight
+import models.Rook
 
-class KnightModelTest extends BaseTestingClass {
+class RookModelTest extends BaseTestingClass {
 
-  var knight: Knight = _
+  var rook: Rook = _
 
   before {
-    knight = new Knight()
+    rook = new Rook()
   }
 
-  test("3x3 board with index = 1 (0,1) - Knight.getAffectedPositionInBoard ") {
+  test("3x3 board with index = 1 (0,1) - Rook.getAffectedPositionInBoard ") {
     val height: Int = 3
     val width: Int = 3
     val index: Int = 1
-    val expectedResult: List[Int] = List(6, 8)
+    val expectedResult: List[Int] = List(0, 2, 4, 7)
     val mappingArray: (Map[String, Int], Map[Int, String]) = initMappingArray(height, width)
     val boardDimensions: (Int, Int) = (height, width)
-    val affectedPositions: List[Int] = knight.getAffectedPositionInBoard(index, mappingArray, boardDimensions)
+    val affectedPositions: List[Int] = rook.getAffectedPositionInBoard(index, mappingArray, boardDimensions)
     val diff1 = expectedResult.diff(affectedPositions)
     val diff2 = affectedPositions.diff(expectedResult)
     assert(diff1.isEmpty && diff2.isEmpty)
   }
 
-  test("3x3 board with index = 4 (1,1) - Knight.getAffectedPositionInBoard ") {
+  test("3x3 board with index = 4 (1,1) - Rook.getAffectedPositionInBoard ") {
     val height: Int = 3
     val width: Int = 3
     val index: Int = 4
-    val expectedResult: List[Int] = List()
+    val expectedResult: List[Int] = List(1, 3, 5, 7)
     val mappingArray: (Map[String, Int], Map[Int, String]) = initMappingArray(height, width)
     val boardDimensions: (Int, Int) = (height, width)
-    val affectedPositions: List[Int] = knight.getAffectedPositionInBoard(index, mappingArray, boardDimensions)
+    val affectedPositions: List[Int] = rook.getAffectedPositionInBoard(index, mappingArray, boardDimensions)
     val diff1 = affectedPositions.diff(expectedResult)
     val diff2 = expectedResult.diff(affectedPositions)
     assert(diff1.isEmpty && diff2.isEmpty)
   }
 
-  test("4x4 board with index = 0 (0,0) - Knight.getAffectedPositionInBoard ") {
+  test("4x4 board with index = 0 (0,0) - Rook.getAffectedPositionInBoard ") {
     val height: Int = 4
     val width: Int = 4
     val index: Int = 0
-    val expectedResult: List[Int] = List(9, 6)
+    val expectedResult: List[Int] = List(1, 2, 3, 4, 8, 12)
     val mappingArray: (Map[String, Int], Map[Int, String]) = initMappingArray(height, width)
     val boardDimensions: (Int, Int) = (height, width)
-    val affectedPositions: List[Int] = knight.getAffectedPositionInBoard(index, mappingArray, boardDimensions)
+    val affectedPositions: List[Int] = rook.getAffectedPositionInBoard(index, mappingArray, boardDimensions)
     val diff1 = affectedPositions.diff(expectedResult)
     val diff2 = expectedResult.diff(affectedPositions)
     assert(diff1.isEmpty && diff2.isEmpty)
   }
 
-  test("Knight.signature is N") {
-    assert(knight.signature == "N")
+  test("Rook.signature is R") {
+    assert(rook.signature == "R")
   }
 
-  test("Knight.checkPosition is true") {
+  test("Rook.checkPosition is true") {
     var board = initBoard(3, 3)
-    var result = knight.checkPosition(1, board)
+    var result = rook.checkPosition(1, board)
     assert(result)
   }
 
-  test("Knight.checkPosition is false") {
+  test("Rook.checkPosition is false") {
     var board = initBoard(3, 3)
     board += (2 -> "Q")
-    var result = knight.checkPosition(2, board)
+    var result = rook.checkPosition(2, board)
     assert(!result)
   }
 
-  test("Knight.validate is true") {
-    val height: Int = 4
-    val width: Int = 4
-    val index: Int = 0
-    val mappingArray: (Map[String, Int], Map[Int, String]) = initMappingArray(height, width)
-    val boardDimensions: (Int, Int) = (height, width)
-    var board = initBoard(height, width)
-    board += (4 -> "K")
-
-    val result = knight.validate(board, index, boardDimensions, mappingArray)
-    assert(result)
-  }
-
-  test("Knight.validate is false") {
+  test("Rook.validate is true") {
     val height: Int = 4
     val width: Int = 4
     val index: Int = 0
@@ -86,11 +73,24 @@ class KnightModelTest extends BaseTestingClass {
     var board = initBoard(height, width)
     board += (6 -> "K")
 
-    val result = knight.validate(board, index, boardDimensions, mappingArray)
+    val result = rook.validate(board, index, boardDimensions, mappingArray)
+    assert(result)
+  }
+
+  test("Rook.validate is false") {
+    val height: Int = 4
+    val width: Int = 4
+    val index: Int = 0
+    val mappingArray: (Map[String, Int], Map[Int, String]) = initMappingArray(height, width)
+    val boardDimensions: (Int, Int) = (height, width)
+    var board = initBoard(height, width)
+    board += (4 -> "K")
+
+    val result = rook.validate(board, index, boardDimensions, mappingArray)
     assert(!result)
   }
 
-  test("Knight.updateBoard") {
+  test("Rook.updateBoard") {
     val height: Int = 4
     val width: Int = 4
     val index: Int = 0
@@ -98,13 +98,17 @@ class KnightModelTest extends BaseTestingClass {
     val boardDimensions: (Int, Int) = (height, width)
     var board = initBoard(height, width)
 
-    val result = knight.updateBoard(board, index, boardDimensions, mappingArray)
+    val result = rook.updateBoard(board, index, boardDimensions, mappingArray)
 
     var expectedResult: Map[Int, String] = board
 
-    expectedResult += (0 -> "N")
-    expectedResult += (6 -> null)
-    expectedResult += (9 -> null)
+    expectedResult += (0 -> "R")
+    expectedResult += (1 -> null)
+    expectedResult += (2 -> null)
+    expectedResult += (3 -> null)
+    expectedResult += (4 -> null)
+    expectedResult += (8 -> null)
+    expectedResult += (12 -> null)
 
     assert(result == expectedResult)
   }
